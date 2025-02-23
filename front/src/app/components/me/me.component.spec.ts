76eb411a83aa118dd00d3644459b53f62ed948b7
joel Dumortier <joel.dumortier@gmail.com>
2025-02-23 08:00:20 +0100

    add unit tests for MeComponent

--- front/src/app/components/me/me.component.spec.ts
+++ front/src/app/components/me/me.component.spec.ts
@@ -4,21 +4,30 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { SessionService } from 'src/app/services/session.service';
+import { expect } from '@jest/globals';
 
 import { MeComponent } from './me.component';
+import { Router } from '@angular/router';
+import { SessionInformation } from 'src/app/interfaces/sessionInformation.interface';
+import { UserService } from 'src/app/services/user.service';
+import { of } from 'rxjs';
 
 describe('MeComponent', () => {
-  let component: MeComponent;
+  let meComponent: MeComponent;
   let fixture: ComponentFixture<MeComponent>;
+  let userService: UserService;
+  let router: Router;
+  let matSnackBar: MatSnackBar;
 
   const mockSessionService = {
     sessionInformation: {
       admin: true,
-      id: 1
-    }
-  }
+      id: 1,
+    },
+    logOut: jest.fn(),
+  };
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [MeComponent],
@@ -28,18 +37,42 @@ describe('MeComponent', () => {
         MatCardModule,
         MatFormFieldModule,
         MatIconModule,
-        MatInputModule
+        MatInputModule,
       ],
       providers: [{ provide: SessionService, useValue: mockSessionService }],
-    })
-      .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(MeComponent);
-    component = fixture.componentInstance;
+    meComponent = fixture.componentInstance;
     fixture.detectChanges();
+    userService = TestBed.inject(UserService);
+    router = TestBed.inject(Router);
+    matSnackBar = TestBed.inject(MatSnackBar);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create MeComponent', () => {
+    expect(meComponent).toBeTruthy();
+  });
+
+
+  it('should delete the user', () => {
+    const userSpy = jest
+      .spyOn(userService, 'delete')
+      .mockImplementation(() => of(undefined));
+
+    const matSnackBarSpy = jest.spyOn(matSnackBar, 'open').mockImplementation();
+
+    const sessionSpy = jest.spyOn(mockSessionService, 'logOut');
+
+    const routerSpy = jest
+      .spyOn(router, 'navigate')
+      .mockImplementation(() => Promise.resolve(true));
+
+    meComponent.delete();
+
+    expect(userSpy).toHaveBeenCalledWith(mockSessionService.sessionInformation.id.toString());
+    expect(matSnackBarSpy).toHaveBeenCalledWith("Your account has been deleted !", "Close", {"duration": 3000});
+    expect(sessionSpy).toHaveBeenCalled();
+    expect(routerSpy).toHaveBeenCalledWith(['/']);
   });
 });
