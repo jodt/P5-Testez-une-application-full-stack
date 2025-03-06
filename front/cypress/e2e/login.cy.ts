f3a0ddb04aaaa72429c5a0d8a0312a8af30891f2
joel Dumortier <joel.dumortier@gmail.com>
2025-03-06 15:53:24 +0100

    add e2e tests for login component

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -1,6 +1,7 @@
 describe('Login spec', () => {
   it('Login successfull', () => {
-    cy.visit('/login')
+
+    cy.visit('/login');
 
     cy.intercept('POST', '/api/auth/login', {
       body: {
@@ -8,20 +9,51 @@ describe('Login spec', () => {
         username: 'userName',
         firstName: 'firstName',
         lastName: 'lastName',
-        admin: true
+        admin: true,
       },
-    })
+    });
 
     cy.intercept(
       {
         method: 'GET',
         url: '/api/session',
       },
-      []).as('session')
+      []
+    );
 
-    cy.get('input[formControlName=email]').type("yoga@studio.com")
-    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+    cy.get('input[formControlName=email]').type('yoga@studio.com');
+    cy.get('input[formControlName=password]').type(`${'test!1234'}{enter}{enter}`);
 
-    cy.url().should('include', '/sessions')
-  })
+    cy.url().should('include', '/sessions');
+  });
+
+
+  it('should disable Submit button if a required field is not filled', () => {
+    cy.visit('/login');
+
+    cy.get('input[formControlName=password]').type(`${'test!1234'}`);
+
+    cy.get('button[type=submit]').should('be.disabled');
+  });
+  
+
+  it('should display an error message if the credentials are wrong ', () => {
+    const errorMessage = 'An error occurred';
+
+    cy.visit('/login');
+
+    cy.intercept('POST', '/api/auth/login', {
+      statusCode: 400,
+      body: {},
+    });
+
+    cy.get('input[formControlName=email]').type('yoga@studio.com');
+    cy.get('input[formControlName=password]').type(
+      `${'test!1234'}{enter}{enter}`
+    );
+    cy.get('button[type=submit]').click();
+
+    cy.url().should('include', '/login');
+    cy.contains(errorMessage).should('be.visible');
+  });
 });
